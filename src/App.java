a73e0b0e36220129aee937ff00b4ad562b9048a8
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-09-29 18:12:44 +0200

    Piramide echa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("%6s", "*\n");
+        System.out.printf("%7s", "***\n");
+        System.out.printf("%8s", "*****\n");
+        System.out.printf("%9s", "*******\n");
+        System.out.printf("%s", "*********\n");
     }
 }
